2a0180940f7836338633eb8072b85f6d64442774
Maciej Kowalski <maciej.kowalski@allegro.pl>
2018-05-17 19:56:19 +0200

    monoid impl

--- src/main/scala/fp/typeclasses/Monoid.scala
+++ src/main/scala/fp/typeclasses/Monoid.scala
@@ -5,5 +5,9 @@ trait Monoid[A] extends Semigroup[A] {
 }
 
 object Monoid {
-  implicit val monoidString: Monoid[String] = ???
+  implicit val monoidString: Monoid[String] = new Monoid[String] {
+    override def empty: String = ""
+
+    override def combine(x: String, y: String): String = x + y
+  }
 }
