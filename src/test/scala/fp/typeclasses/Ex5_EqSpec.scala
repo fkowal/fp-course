197ece1ab09c7d56b8e77f2e3cd0d301e5e969af
Maciej Kowalski <maciej.kowalski@allegro.pl>
2019-01-20 18:20:38 +0100

    ordered excercises

--- src/test/scala/fp/typeclasses/Ex5_EqSpec.scala
+++ src/test/scala/fp/typeclasses/Ex5_EqSpec.scala
@@ -2,7 +2,7 @@ package fp.typeclasses
 
 import org.scalatest.{FlatSpec, Matchers}
 
-class EqSpec extends FlatSpec with Matchers {
+class Ex5_EqSpec extends FlatSpec with Matchers {
   it should "work" in {
     assert(exerciseEq.check(1) === "It is one!")
     assert(exerciseEq.check(2) === "It is something else then 1")
