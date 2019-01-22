1209013af184f506bd6f10b11070641f82d7f44a
Maciej Kowalski <maciej.kowalski@allegro.pl>
2019-01-22 22:22:42 +0100

    idspec uncomment

--- src/test/scala/fp/typeclasses/IdSpec.scala
+++ src/test/scala/fp/typeclasses/IdSpec.scala
@@ -12,26 +12,26 @@ class IdSpec extends FlatSpec with Matchers {
     val idInt: Id[Int] = 3
 
     "idInt.map(_ + 1) == 4" should compile
-//    assert(idInt.map(_ + 1) == 4) // uncomment me after you've implemented the monad
+    assert(idInt.map(_ + 1) == 4) // uncomment me after you've implemented the monad
   }
 
 // uncomment after above test passes
 
-//  it should "support flatMapping" in {
-//    def fn(i: Int): Id[Int] = i + 1
-//
-//    val result: (Int, Int) = for {
-//      i <- fn(0)
-//      j <- fn(i)
-//    } yield (i, j)
-//
-//    assert(result._1 == 1)
-//    assert(result._2 == 2)
-//  }
-//
-//  it should "support Applicative[Id].pure" in {
-//    val idInt: Id[Int] = Applicative[Id].pure(123)
-//
-//    assert(idInt == 123)
-//  }
+  it should "support flatMapping" in {
+    def fn(i: Int): Id[Int] = i + 1
+
+    val result: (Int, Int) = for {
+      i <- fn(0)
+      j <- fn(i)
+    } yield (i, j)
+
+    assert(result._1 == 1)
+    assert(result._2 == 2)
+  }
+
+  it should "support Applicative[Id].pure" in {
+    val idInt: Id[Int] = Applicative[Id].pure(123)
+
+    assert(idInt == 123)
+  }
 }
